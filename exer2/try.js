18aac88cb58116a0a0eb180024f6539bd09c4c77
AldCristobal <alcristobal1@up.edu.ph>
2024-03-01 10:23:27 +0800

    added exer folder partitions

---
+++ exer2/try.js
@@ -0,0 +1,22 @@
+﻿console.log("Hello World")
+
+
+//for validating if 2 passwords match, accepting 2 string params
+//>if 2 passwords match
+//>has atleast 8 characters
+//>has atleast 1 number, 1 uppercase, and 1 lowercase
+//else, return false
+function passValidation(pw1, pw2) {
+
+}
+
+//returns the reversed string pw
+function passReversion(pw) {
+
+}
+
+//
+function passToObj(name, pw1, pw2) {
+
+}
+
