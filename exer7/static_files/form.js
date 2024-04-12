e5ff811ad0d21c43b1c4e714e1008ad6abd82e20
AldCristobal <alcristobal1@up.edu.ph>
2024-04-12 12:21:23 +0800

    ADD form validation

--- exer7/static_files/form.js
+++ exer7/static_files/form.js
@@ -1,14 +1,52 @@
 ﻿const submitButton = document.getElementById("submit-foodcard");
-const foodname = document.getElementById("foodname");
-const desc = document.getElementById("desc");
-const imagelink = document.getElementById("imagelink");
-const ranking = document.getElementById("ranking");
+
 
 const validateForm = () => {
-	console.log(foodname);
-	console.log(desc);
-	console.log(imagelink);
-	console.log(ranking);
+	const foodname = document.getElementById("foodname").value;
+	const desc = document.getElementById("desc").value;
+	const imagelink = document.getElementById("imagelink").value;
+	const ranking = document.getElementById("ranking").value;
+	let alertmsg = "Invalid value/s found:";
+
+	const valid_foodname = foodname != "";
+	if(!valid_foodname){
+		alertmsg += "\n >Food name must not be empty."
+	}
+
+	const valid_desc = desc != "";
+	if(!valid_desc){
+		alertmsg += "\n >Description must not be empty."
+	}
+
+	const valid_imagelink = imagelink != "";
+	if(!valid_imagelink){
+		alertmsg += "\n >Image URL must not be empty."
+	}
+
+	let valid_ranking = false;
+	if (ranking == "") {
+		alertmsg += "\n >Food name must not be empty."
+	}
+	else{
+		valid_ranking = Number(ranking) > 0;
+		if(!valid_ranking){
+			alertmsg += "\n >Food name must be a non-zero positive integer."
+		}
+	}
+	
+	if(alertmsg != "Invalid value/s found:"){
+		alert(alertmsg);
+	}
+	else{
+		alert("Valid");
+	}
+
+	console.log({foodname, valid_foodname});
+	console.log({desc, valid_desc});
+	console.log({imagelink, valid_imagelink});
+	console.log({ranking, valid_ranking});
+
+	// alert("Form Validated");
 } 
 
 submitButton.addEventListener("click", validateForm);
